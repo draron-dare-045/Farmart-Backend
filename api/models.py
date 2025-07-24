51549fd42f81f35c75d20d6b26160758f4b2e3b6
Dwayne <dune.njenga@student.moringaschool.com>
2025-07-24 22:03:18 +0300

    Added order model

--- api/models.py
+++ api/models.py
@@ -27,3 +27,25 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.get_user_type_display()})"
+
+
+class Order(models.Model):
+    """Model for customer's orders."""
+    class OrderStatus(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        CONFIRMED = 'CONFIRMED', 'Confirmed'
+        REJECTED = 'REJECTED', 'Rejected'
+        PAID = 'PAID', 'Paid'
+        DELIVERED = 'DELIVERED', 'Delivered'
+
+    buyer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        limit_choices_to={'user_type': User.Types.BUYER}
+    )
+    status = models.CharField(max_length=20, choices=OrderStatus.choices, default=OrderStatus.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.buyer.username} - {self.get_status_display()}"
