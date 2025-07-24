9657d17758860727900be9bb0cd980538fe82206
Dwayne <dune.njenga@student.moringaschool.com>
2025-07-25 00:19:44 +0300

    Updated urls

---
+++ api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    AnimalViewSet,
+    OrderViewSet,
+    MakePaymentView,
+    MpesaCallbackView,
+    UserProfileView,
+    RegisterUserView,
+)
+
+router = DefaultRouter()
+router.register(r'animals', AnimalViewSet, basename='animal')
+router.register(r'orders', OrderViewSet, basename='order')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    
+    path('register/', RegisterUserView.as_view(), name='register-user'),
+    path('users/me/', UserProfileView.as_view(), name='user-profile'),
+    path('make-payment/', MakePaymentView.as_view(), name='make-payment'),
+    path('mpesa-callback/', MpesaCallbackView.as_view(), name='mpesa-callback'),
+]
