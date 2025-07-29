48c766663d294cef55cd92198a3f3e4144b45be9
claire <claire.kimani@student.moringaschool.com>
2025-07-29 12:02:02 +0300

    modified urls.py, requirements

--- farmart_project/urls.py
+++ farmart_project/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls.jwt')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
